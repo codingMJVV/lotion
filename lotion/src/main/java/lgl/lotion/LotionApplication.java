b293041f66cbcc6ee779b2d4ad5cf9e2bb55d3b1
codingMJVV <88642701+codingMJVV@users.noreply.github.com>
2023-04-13 20:13:05 +0900

    Initial commit

---
+++ lotion/src/main/java/lgl/lotion/LotionApplication.java
@@ -0,0 +1,13 @@
+package lgl.lotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotionApplication.class, args);
+	}
+
+}
---
+++ lotion/src/test/java/lgl/lotion/LotionApplicationTests.java
@@ -0,0 +1,13 @@
+package lgl.lotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
